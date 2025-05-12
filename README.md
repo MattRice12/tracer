5558c021f2a0b5cf3a464551705e1188314e9454
Matt Rice <mattricedev@gmail.com>
2025-05-11 23:07:04 -0500

    Created tree-like structure for better visual output

--- README.md
+++ README.md
@@ -4,26 +4,22 @@ Tracer is a script that takes a starting point and prints out all the downstream
 1. To increase the visibility one has for what is affected by their changes, and
 2. To gather the scope of changes one will need to make if they choose to insert a new feature into existing functionality.
 
-# Wants
-
-1. Print out a tree-like display to for a better visualization of the branches a service may take to execute
-
 # Current Output
 ```
-Call paths from ClassA#call:
-ClassA#call() -> #<Class:ClassB>.call(n1=Integer, n2=Integer) -> ClassB#initialize()
-ClassA#call() -> #<Class:ClassB>.call(n1=Integer, n2=Integer) -> ClassB#call(n1=Integer, n2=Integer) -> #<Class:ClassD>.call() -> ClassD#initialize()
-ClassA#call() -> #<Class:ClassB>.call(n1=Integer, n2=Integer) -> ClassB#call(n1=Integer, n2=Integer) -> #<Class:ClassD>.call() -> ClassD#call()
-ClassA#call() -> #<Class:ClassB>.call(n1=Integer, n2=Integer) -> ClassB#call(n1=Integer, n2=Integer) -> #<Class:ClassD>.call()
-ClassA#call() -> #<Class:ClassB>.call(n1=Integer, n2=Integer) -> ClassB#call(n1=Integer, n2=Integer) -> ClassB#product(n1=Integer, n2=Integer)
-ClassA#call() -> #<Class:ClassB>.call(n1=Integer, n2=Integer) -> ClassB#call(n1=Integer, n2=Integer)
-ClassA#call() -> #<Class:ClassB>.call(n1=Integer, n2=Integer)
-ClassA#call() -> #<Class:ClassE>.call() -> ClassE#initialize()
-ClassA#call() -> #<Class:ClassE>.call() -> ClassE#call()
-ClassA#call() -> #<Class:ClassE>.call()
-ClassA#call() -> ClassA#sum()
-ClassA#call()
-
+Call Tree:
+└── class_a.rb:0 `#call`
+    └── class_a.rb:16 `#call`
+        ├── class_b.rb:4 `.call` (n1=Integer, n2=Integer)
+        │   ├── class_b.rb:8 `#initialize`
+        │   └── class_b.rb:11 `#call` (n1=Integer, n2=Integer)
+        │       ├── class_d.rb:2 `.call`
+        │       │   ├── class_d.rb:6 `#initialize`
+        │       │   └── class_d.rb:9 `#call`
+        │       └── class_b.rb:16 `#product` (n1=Integer, n2=Integer)
+        ├── class_e.rb:2 `.call`
+        │   ├── class_e.rb:6 `#initialize`
+        │   └── class_e.rb:9 `#call`
+        └── class_a.rb:22 `#sum`
 ```
 # Author
 me
--- tracer.rb
+++ tracer.rb
@@ -1,3 +1,4 @@
+require_relative "./tree_node.rb"
 require_relative "./class_a.rb"
 
 class CallTracer
@@ -8,43 +9,64 @@ class CallTracer
     @kwargs = kwargs
     @call_stack = []
     @call_paths = []
+    @project_root = Dir.pwd
   end
 
   def trace
+    um = @root_object.class.instance_method(@method_name)
+    file, line = um.source_location
+    root_label = format_method(@root_object.class, @method_name, [], 0, file, instance_method: true)
+    root = TreeNode.new(root_label)
+
     tracer = TracePoint.new(:call, :return) do |tp|
       next unless tp.method_id
 
       if tp.event == :call
         args_str = extract_arguments(tp)
-        delimiter = tp.self.is_a?(Module) ? "." : "#"
-
-        method_str = "#{tp.defined_class}#{delimiter}#{tp.method_id}(#{args_str})"
-        @call_stack.push(method_str)
+        line = tp.lineno
+        instance_method = !tp.self.is_a?(Module)
+        label = format_method(tp.defined_class, tp.method_id, args_str, line, tp.path, instance_method: instance_method)
+        @call_stack.push(label)
       elsif tp.event == :return
-        path = @call_stack.join(" -> ")
-        @call_paths << path unless @call_paths.include?(path)
+        root.add_path(@call_stack.dup)
         @call_stack.pop
       end
     end
 
     tracer.enable
-    @root_object.public_send(@method_name, *@args, **@kwargs)
+    if @root_object.is_a?(Class)
+      @root_object.public_send(@method_name, *@args, **@kwargs)
+    else
+      @root_object.public_send(@method_name, *@args, **@kwargs)
+    end
     tracer.disable
 
-    puts "Call paths from #{@root_object.class}##{@method_name}:"
-    @call_paths.each { |path| puts path }
+    puts "Call Tree:"
+    root.print_tree("", true)
+  end
+
+  def extract_arguments(tp)
+    tp.parameters.map do |type, name|
+      begin
+        value = tp.binding.local_variable_get(name)
+        "#{name}=#{value.class}"
+      rescue NameError
+        "#{name}=<unknown>"
+      end
+    end.join(", ")
+  end
+
+  def format_method(klass, method_name, args_str, line, path = nil, instance_method:)
+    delimiter = instance_method ? "#" : "."
+    args_text = args_str.empty? ? '' : "(#{args_str})"
+    location = path ? "#{relative_path(path)}:#{line}" : "line #{line}"
+    "#{location} `#{delimiter}#{method_name}` #{args_text}"
+  end
+
+  def relative_path(path)
+    path.start_with?(@project_root) ? path.sub(@project_root + "/", "") : path
   end
 end
 
-def extract_arguments(tp)
-  tp.parameters.map do |type, name|
-    begin
-      value = tp.binding.local_variable_get(name)
-      "#{name}=#{value.class}"
-    rescue NameError
-      "#{name}=<unknown>"
-    end
-  end.join(", ")
-end
 
 CallTracer.new(ClassA.new(1, 2), :call).trace
